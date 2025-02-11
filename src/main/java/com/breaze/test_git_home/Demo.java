764ce762de532a006210d79ec8928c4c53aae304
breaze <simon.gaviriao@gmail.com>
2025-02-11 11:40:05 -0500

    Busqueda binaria

--- src/main/java/com/breaze/test_git_home/Demo.java
+++ src/main/java/com/breaze/test_git_home/Demo.java
@@ -11,14 +11,28 @@ package com.breaze.test_git_home;
 public class Demo {
     
     public int buscarNumeroEnArreglo(int[] arreglo, int valorBuscado){
-        // Recorremos el arreglo
-        for (int i = 0; i < arreglo.length; i++) {
-            // Si encontramos el valor, devolvemos el índice
-            if (arreglo[i] == valorBuscado) {
-                return i;
+        int inicio = 0;
+        int fin = arreglo.length - 1;
+
+        while (inicio <= fin) {
+            int medio = inicio + (fin - inicio) / 2;
+
+            // Verifica si la clave está en el medio
+            if (arreglo[medio] == clave) {
+                return medio;
+            }
+
+            // Si la clave es mayor, ignora la mitad izquierda
+            if (arreglo[medio] < clave) {
+                inicio = medio + 1;
+            } 
+            // Si la clave es menor, ignora la mitad derecha
+            else {
+                fin = medio - 1;
             }
         }
-        // Si no se encuentra el valor, devolvemos -1
+
+        // Si no se encontró la clave
         return -1;
     }
     
