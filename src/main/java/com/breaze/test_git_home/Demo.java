517616c82b188ba5409a6ad6fa21ed5d457e49a3
breaze <simon.gaviriao@gmail.com>
2025-02-11 12:36:06 -0500

    Probando version final

--- src/main/java/com/breaze/test_git_home/Demo.java
+++ src/main/java/com/breaze/test_git_home/Demo.java
@@ -11,28 +11,12 @@ package com.breaze.test_git_home;
 public class Demo {
     
     public int buscarNumeroEnArreglo(int[] arreglo, int valorBuscado){
-        System.out.println("hola juan pedro, y mis otros cambios. Este es un mensaje y demas codigo");
-        int inicio = 0;
-        int fin = arreglo.length - 1;
-
-        while (inicio <= fin) {
-            int medio = inicio + (fin - inicio) / 2;
-
-            // Verifica si la clave está en el medio
-            if (arreglo[medio] == valorBuscado) {
-                return medio;
-            }
-
-            // Si la clave es mayor, ignora la mitad izquierda
-            if (arreglo[medio] < valorBuscado) {
-                inicio = medio + 1;
-            } 
-            // Si la clave es menor, ignora la mitad derecha
-            else {
-                fin = medio - 2;
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == valorBuscado) {
+                return i; // Devuelve la posición donde se encontró la clave
             }
         }
-        return -1; // Si no se encontró
+        return -1;
     }
     
 }
